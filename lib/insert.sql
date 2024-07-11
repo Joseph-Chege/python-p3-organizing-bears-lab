97d74a3b37d2cb4f84bbaeca67cc7d1397c3f667
Joseph-Chege <mjchege23@gmail.com>
2024-07-11 16:27:34 +0300

    Queried using SQL commands

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'Male', 'Brown', 'Grumpy', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 12, 'Male', 'Black', 'Quiet', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 9, 'Female', 'Brown', 'Sweet', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'Male', 'Brown', 'Hostile', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'Female', 'Black', 'Sleepy', 'False');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'Male', 'Brown', 'Sour', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 10, 'Female', 'White', 'Energetic', 'True');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, 'Female', 'White', 'Goofy', 'True');
